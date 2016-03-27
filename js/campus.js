227a49adf09333f8edd1d5286d148195e4f2341b
mikarin <margarita.mikarin@gmail.com>
2016-03-27 11:11:12 +0200

    new info_boxes, new hover buttons

--- js/campus.js
+++ js/campus.js
@@ -7,47 +7,55 @@ function Viewpoint(id, position, orientation) {
   this.orientation = orientation;
 }
 
-function Item(label, humanViewpoint, birdViewpoint) {
+function Item(label, humanViewpoint, birdViewpoint, title) {
   this.label = label;
   this.humanViewpoint = humanViewpoint;
   this.birdViewpoint = birdViewpoint;
+  this.title = title;
 }
 
 var items = [
   new Item(
     "Main View",
     new Viewpoint("main_viewpoint-human", "-0.76461 0.33955 11.39530", "-0.78724 0.61627 0.02182 0.08991"),  // от-к ректорату, высота, от-к витезнему намнести
-    new Viewpoint("main_viewpoint-bird", "-8.88020 18.60237 13.51623", "-0.78298 -0.56043 -0.26993 1.10296")
+    new Viewpoint("main_viewpoint-bird", "-8.88020 18.60237 13.51623", "-0.78298 -0.56043 -0.26993 1.10296"),
+    "Vitezne Square"
   ),
   new Item(
-    "FEL",
+    "FEE",
     new Viewpoint("fel_human", "-1.15427 0.36467 7.81001", "-0.03665 -0.99870 -0.03537 1.53646"),
-    new Viewpoint("fel_bird", "-2.17067 5.24240 10.78380", "-0.66205 -0.68064 -0.31373 1.21638")
+    new Viewpoint("fel_bird", "-2.17067 5.24240 10.78380", "-0.66205 -0.68064 -0.31373 1.21638"),
+    "Faculty of Electrical Engineering"
   ),
   new Item(
-    "FS",
+    "FME",
     new Viewpoint("stroj_human", "-1.15427 0.36467 2.76583", "-0.03488 -0.99877 -0.03537 1.58590"),
-    new Viewpoint("stroj_bird", "-2.99693 5.59042 -0.25765", "0.19969 0.90957 0.36443 4.06632")
+    new Viewpoint("stroj_bird", "-2.99693 5.59042 -0.25765", "0.19969 0.90957 0.36443 4.06632"),
+    "Faculty of Mechanicak Engineering"
   ),
   new Item(
-    "NTK",
+    "NLT",
     new Viewpoint("ntk_human", "-1.16524 0.36467 0.45499", "-0.06312 -0.99738 -0.03532 1.02260"),
-    new Viewpoint("ntk_bird", "-2.54777 6.77093 2.00792", "-0.67601 -0.66711 -0.31300 1.21343")
+    new Viewpoint("ntk_bird", "-2.54777 6.77093 2.00792", "-0.67601 -0.66711 -0.31300 1.21343"),
+    "National Library of Technology"
   ),
   new Item(
-    "FSv",
+    "FCE",
     new Viewpoint("stavebni_human", "-0.63949 0.36467 -3.62284", "-0.93547 0.35315 0.01370 0.08292"),
-    new Viewpoint("stavebni_bird", "-4.98515 6.94844 0.21307", "-0.79983 -0.57433 -0.17441 0.72574")
+    new Viewpoint("stavebni_bird", "-4.98515 6.94844 0.21307", "-0.79983 -0.57433 -0.17441 0.72574"),
+    "Faculty of Civil Engineering"
   ),
   new Item(
     "FIT/FA",
     new Viewpoint("fit_human", "3.08990 0.36467 -3.62284", "-0.95328 0.30191 0.01069 0.07426"),
-    new Viewpoint("fit_bird", "0.10475 10.19714 -1.42952", "-0.85969 -0.44993 -0.24185 1.11757")
+    new Viewpoint("fit_bird", "0.10475 10.19714 -1.42952", "-0.85969 -0.44993 -0.24185 1.11757"),
+    "Faculty of Information Technology"
   ),
   new Item(
-    "RCVUT",
+    "ACTU",
     new Viewpoint("rektorat_human", "9.69538 0.36467 9.41136", "-0.08213 0.99600 0.03527 0.81423"),
-    new Viewpoint("rektorat_bird", "9.26513 7.19641 15.86503", "-0.97437 0.20566 0.09116 0.85385")
+    new Viewpoint("rektorat_bird", "9.26513 7.19641 15.86503", "-0.97437 0.20566 0.09116 0.85385"),
+    "New CTU Administration"
   )
 ];
 
@@ -64,6 +72,7 @@ function render() {
 
     var label = document.createElement("label");
     label.innerHTML = item.label;
+    label.setAttribute('title', item.title);
     li.appendChild(label);
 
     var img_div = document.createElement('span');
@@ -158,7 +167,7 @@ function viewFunc(evt) {        // show viewpoint values
         camRot = rot[0].x.toFixed(4)+' '+rot[0].y.toFixed(4)+' '+rot[0].z.toFixed(4)+' '+rot[1].toFixed(4);
         //document.getElementById("viewMat").innerHTML = "&ltViewpoint position='" + camPos + "' orientation='" + camRot + "'&gt";
       }
-      //annotationtest();
+      annotationtest();
         // update 2d marker also if camera changes since projection is now wrong
         /*var trans = x3dom.fields.SFVec3f.parse(document.getElementById('bar').getAttribute("translation"));
 	      var pos2d = runtime.calcPagePos(trans.x, trans.y, trans.z);
@@ -302,8 +311,8 @@ function eventAdd() {
   table.addEventListener('mouseout', function() {
     document.getElementById("scene__terrain_texture").setAttribute('url', 'map.png');
   }, false);*/
-  //createAnnotationWindows();
-  //annotationtest();
+  createAnnotationWindows();
+  annotationtest();
   objectsAnnotations();
   document.getElementById("tmp_viewpoint").addEventListener('viewpointChanged', viewFunc, false);
 }
@@ -312,13 +321,13 @@ function rotate_map(value) {
   var rotation_angle = parseFloat($('#scene_rotation').attr('rotation').split(',')[3]);
   rotation_angle += 0.5 * value;
   $('#scene_rotation').attr('rotation','0,1,0,' + rotation_angle);
-  //annotationtest();
+  annotationtest();
 }
 
 function zoom(delta) {
 	var vpt = document.getElementById("main_viewpoint-bird");
 	vpt.fieldOfView = parseFloat(vpt.fieldOfView) + delta;
-  //annotationtest();
+  annotationtest();
 }
 
 var buildings = [
